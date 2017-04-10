2f5bd2e345b96f04c36daf03bcddf4fd667b92b7
chaim <chaim@chaimimac.local>
2017-04-10 18:55:29 +0800

    add network test

---
+++ network/Process.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace network
+{
+	public class Process
+	{
+		public delegate void ProcessDelegate (IProtoObject content);
+
+		public static  Dictionary<string, ProcessDelegate> Processlist = new Dictionary<string, ProcessDelegate> ();
+		public Dictionary<string, IProtoObject> ObjectListIn = new Dictionary<string, IProtoObject> ();
+		public Dictionary<string, IProtoObject> ObjectListOut = new Dictionary<string, IProtoObject> ();
+
+		public Process ()
+		{
+			ObjectListIn.Add ("0.0", new LoginIn ());
+			ObjectListIn.Add ("0.1", new RankIn ());
+
+			ObjectListOut.Add ("0.0", new LoginOut ());
+			ObjectListOut.Add ("0.1", new RankOut ());
+
+			Processlist.Add ("0.0", LoginOutProcess);
+		}
+
+
+		public void Send (IProtoObject data)
+		{
+			try {
+				byte[] buffer = MarshalData (data);
+				MainClass.Logarr (buffer);
+			} catch (Exception ex) {
+				Console.WriteLine (ex.Message);
+			}
+		}
+
+		public  void DoProcess (string key)
+		{
+			IProtoObject content = ObjectListOut [key];
+			byte[] buffer = new byte[content.Size ()];
+			content.Unmarshal (buffer, 0);
+		
+			if (Processlist.ContainsKey (key)) {
+				Processlist [key] (content);
+			}
+		}
+
+		public byte[] MarshalData (IProtoObject data)
+		{
+			byte[] buffer = new byte[data.Size ()];
+			data.Marshal (buffer, 0);
+			return buffer;
+		}
+
+		public void UnMarshalData (byte[] buffer, IProtoObject data)
+		{
+			data.Unmarshal (buffer, 0);
+		}
+
+		public void LoginOutProcess (IProtoObject content)
+		{
+			Console.WriteLine ("LoginOutProcess" + (LoginOut)content);
+		}
+
+		public string GetProperties<T> (T t)
+		{
+			string tStr = string.Empty;
+			if (t == null) {
+				return tStr;
+			}
+			System.Reflection.PropertyInfo[] properties = t.GetType ().GetProperties (System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
+
+			if (properties.Length <= 0) {
+				return tStr;
+			}
+			foreach (System.Reflection.PropertyInfo item in properties) {
+				string name = item.Name;
+				object value = item.GetValue (t, null);
+				if (item.PropertyType.IsValueType || item.PropertyType.Name.StartsWith ("String")) {
+					tStr += string.Format ("{0}:{1},", name, value);
+				} else {
+					GetProperties (value);
+				}
+			}
+			return tStr;
+		}
+	}
+}
+
---
+++ network/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+namespace network
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Process p = new Process ();
+
+
+			LoginIn li = new LoginIn ();
+			li.User = System.Text.Encoding.Default.GetBytes ("abc");
+			li.Group = LoginGroup2.LOGIN_GROUP_E;
+
+			byte[] bytes = p.MarshalData (li);
+			Logarr (bytes);
+
+			IProtoObject uobject = new LoginIn ();
+			p.UnMarshalData (bytes, uobject);
+			Console.WriteLine (((LoginIn)uobject).Group);
+
+//			IProtoObject v = p.ObjectListIn ["0.0"];
+//			LoginIn v2 = (LoginIn)v;
+//			Console.WriteLine ("abc" + v2);
+//			Console.WriteLine("abc"+ p.GetProperties<LoginIn> (v));
+		}
+
+		public static void Logarr (byte[] bytes)
+		{
+			Console.Write ("(" + bytes.Length + ")");
+			for (int i = 0; i < bytes.Length; i++) {
+				Console.Write (bytes [i] + " ");
+			}
+			Console.WriteLine ("");
+		}
+
+
+
+
+	}
+}
